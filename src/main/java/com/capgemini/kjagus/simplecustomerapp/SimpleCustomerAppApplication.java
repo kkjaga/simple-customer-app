1a174adfdc6865cf8317ac934fad1d2743e75d11
kkjaga <kk.jaga@yahoo.com>
2023-04-12 21:38:49 +0200

    Add initial project classes and build configuration

---
+++ src/main/java/com/capgemini/kjagus/simplecustomerapp/SimpleCustomerAppApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.kjagus.simplecustomerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCustomerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCustomerAppApplication.class, args);
+	}
+
+}
